11eb0811f2d499f9deea182832a6fba38e2c1cae
Kalzhan-learner <serikbayevkalzhan0@gmail.com>
2025-01-18 15:39:51 +0500

    asteroid

--- circleshape.py
+++ circleshape.py
@@ -13,3 +13,13 @@ class PlayerCircle(CircleShape):
     def update(self, dt):
         # Обновляем позицию или другие параметры (например, по скорости)
         self.position += self.velocity * dt
+
+class Player(CircleShape):
+    def __init__(self, x, y, radius):
+        super().__init__(x, y, radius)
+
+    def draw(self, screen):
+        pygame.draw.circle(screen, (0, 255, 0), (int(self.position.x), int(self.position.y)), self.radius)
+
+    def update(self, dt):
+        self.position += self.velocity * dt
